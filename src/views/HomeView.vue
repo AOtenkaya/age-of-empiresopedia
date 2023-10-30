90327e86efacf59246663e48375a17e8e02896e3
AOtenkaya <otenkayadev@gmail.com>
2023-10-30 22:49:47 +0300

    feat: shape HomeView and create UnitDetailView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,30 @@
 <template>
-  <HelloWorld />
+  <div class="home">
+    <img class="home__image" :src="require('@/assets/images/homepage.jpg')"/>
+  </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 
 // Components
-import HelloWorld from "../components/HelloWorld.vue";
 
 export default defineComponent({
   name: "HomeView",
-
-  components: {
-    HelloWorld,
-  },
 });
 </script>
+
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+
+  &__image {
+    max-width: 100%;
+    height: 50%;
+    object-fit: contain;
+  }
+}
+</style>
---
+++ src/views/UnitDetailView.vue
@@ -0,0 +1,19 @@
+<template>
+  <unit-details-container/>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+// Containers
+import UnitDetailsContainer from '@/containers/UnitDetailsContainer'
+
+export default defineComponent({
+  name: "UnitDetailView",
+
+  components: {
+    UnitDetailsContainer,
+  },
+});
+</script>
+
