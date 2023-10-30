daf5f2fe24d7058870c716aedfdac12783501a46
AOtenkaya <otenkayadev@gmail.com>
2023-10-30 22:46:43 +0300

    feat: shape app vue and make router settings

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
   <v-app>
     <v-main>
+      <navigation-bar/>
+
       <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
+import NavigationBar from "@/containers/NavigationBar.vue";
+
 export default {
   name: "App",
+
+  components: {
+    NavigationBar,
+  },
 
   data: () => ({
     //
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import UnitListView from "../views/UnitListView.vue";
+import UnitDetailView from "../views/UnitDetailView.vue";
 
 const routes = [
   {
@@ -8,14 +10,16 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/units",
+    name: "unitList",
+    component: UnitListView,
   },
+  {
+    path: "/units/:id",
+    name: "unitDetail",
+    component: UnitDetailView,
+  },
+
 ];
 
 const router = createRouter({
